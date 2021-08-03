a9c6e8b747baa67654bf8e55f7ff3e125b491f14
Martin Rouault <martin.rouault@ensae.fr>
2021-08-03 10:46:34 +0200

    Cross validation nugget

--- importance_sampling.py
+++ importance_sampling.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy
 from scipy.stats import invgamma, gamma
+from sklearn.model_selection import train_test_split, GridSearchCV
 
 class importance :
 
@@ -9,7 +10,7 @@ class importance :
 
         self.N = N
         self.alpha = 1 if not bridge else alpha 
-        self.sig2 = [sig2 for k in range(N)]
+        self.sig2 = sig2
         self.lamb = lamb
         self.nugget = nugget
         self.ksig2 = ksig2
@@ -33,17 +34,17 @@ class importance :
             self.sig2 = invgamma.rvs(a = a, scale = self.thetasig2+0.5*Yz, size = self.N)
         self.beta = np.empty((self.N, self.p))
         mean = scipy.linalg.solve_triangular(C.T, Cmean, lower = False)
+        if not self.lamb_known :
+            self.lamb = np.empty(self.N)
         for i in range(self.N):
             z = np.random.randn(self.p)
             Cvar = scipy.linalg.solve_triangular(C.T, z, lower = False)
-            self.beta[i, :] = (mean+np.sqrt(self.sig2[i])*Cvar)
-        if not self.lamb_known :
-            self.lamb = np.empty(self.N)  
-            for i in range(self.N): 
-                b_ = 1 if self.bridge else 1/np.sqrt(self.sig2[i])
+            sig_ = self.sig2 if self.sig2_known else self.sig2[i]
+            self.beta[i, :] = (mean+np.sqrt(sig_)*Cvar) 
+            if not self.lamb_known : 
+                b_ = 1 if self.bridge else 1/np.sqrt(sig_) 
                 self.lamb[i] = gamma.rvs(a = self.klambda + self.p/self.alpha,
                         scale = 1/(self.thetalambda+b_*np.sum(np.abs(self.beta[i, :])**self.alpha)), size = 1)
-            self.tau = self.lamb**(-1/self.alpha)
         return(self)
 
     def weight(self):
@@ -51,11 +52,12 @@ class importance :
         lw = np.empty(self.N)
         for i in range(self.N):
             zi = np.abs(self.beta[i, :])
-            bi_ = 1 if self.bridge else np.sqrt(self.sig2[i])
+            sig_ = self.sig2 if self.sig2_known else self.sig2[i]
+            bi_ = 1 if self.bridge else np.sqrt(sig_)
             if self.lamb_known :
-                lw[i] = -self.lamb * np.sum(zi**self.alpha)/bi_ + self.nugget * np.linalg.norm(self.beta[i, :], ord = 2)/self.sig2[i]
+                lw[i] = -self.lamb * np.sum(zi**self.alpha)/bi_ + self.nugget * np.linalg.norm(self.beta[i, :], ord = 2)/sig_
             else :
-                lw[i] = -(self.p/self.alpha+self.klambda)*np.log(self.thetalambda+np.sum(zi)/bi_) + self.nugget*np.linalg.norm(self.beta[i, :], ord = 2)/self.sig2[i]
+                lw[i] = -(self.p/self.alpha+self.klambda)*np.log(self.thetalambda+np.sum(zi)/bi_) + self.nugget*np.linalg.norm(self.beta[i, :], ord = 2)/sig_
         self.w = np.exp(lw - np.max(lw))
         self.w /= np.sum(self.w)
         self.ess = np.sum(self.w)**2 / np.sum(self.w**2)
@@ -85,4 +87,22 @@ class importance :
 
         for parameter, value in parameters.items():
             setattr(self, parameter, value)
+        return self
+
+    def cvnugget(self, X, Y):
+
+        self.X_ = X
+        self.y_ = Y
+        self.n, self.p = X.shape 
+        X_train, X_test, Y_train, Y_test = train_test_split(self.X_, self.y_, test_size = 0.3, random_state =0)
+        nugmin = -5
+        if np.all(np.linalg.eigvals(self.X_.T.dot(self.X_)) > 0):
+            print("Matrix is positive definite, nugget can be set to 0")
+        else :
+            while not np.all(np.linalg.eigvals(self.X_.T.dot(self.X_) + (10**(nugmin))*np.eye(self.p))> 0):
+                nugmin+=1
+        parameters = {'nugget' : [10**nug for nug in range(nugmin, nugmin + 7)]}
+        cv = GridSearchCV(self, parameters, scoring = "neg_mean_squared_error")
+        cv.fit(X_train, Y_train)
+        self.nugget = cv.best_params_['nugget']
         return self
